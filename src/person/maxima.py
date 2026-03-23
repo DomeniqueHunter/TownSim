db3ab0798c1fb26537fe81162320d02fef3ffa04
dome <domeniquehunterboobs@gmail.com>
2026-03-23 20:44:57 +0100

    Maxima in own class as example for special people

--- src/person/maxima.py
+++ src/person/maxima.py
@@ -4,4 +4,13 @@ from stats import Attributes
 
 import math
 
-maxima = Person(name="Maxima", attributes=Attributes(health=(1000, 1000), stamina=(1000, 1000), hunger=(0, 10_000), money=math.inf))
+# maxima = Person(name="Maxima", attributes=Attributes(health=(1000, 1000), stamina=(1000, 1000), hunger=(0, 10_000), money=math.inf))
+
+class Maxima(Person):
+    
+    def __init__(self, name:str="Maxima", attributes=Attributes(health=(1000, 1000), stamina=(1000, 1000), hunger=(0, 10_000), money=math.inf)):
+        Person.__init__(self)
+        self.name = name
+        self.attributes = attributes
+        
+maxima = Maxima()
